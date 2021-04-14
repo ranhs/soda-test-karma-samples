f54e5013835643f475c18e9d9969d379b02e8a47
Ran Har-Shuv <ran.harshuv@gmail.com>
2021-04-15 02:12:38 +0300

    v1.0.10

--- src/test-samples/utils.spec.ts
+++ src/test-samples/utils.spec.ts
@@ -1,22 +1,24 @@
 import { describe, SinonStub, stub, it, TR, expect, context, beforeEach } from 'soda-test'
 
-import * as crypto from 'crypto'
-import { getHash, isString } from './utils'
+import { isString } from './utils'
 
 @describe('utils')
 class UtilsTest {
 
-    /* does not support stubbing buildin libraray (crypto) */
-// 
-// TBD: check way this direct stubbing does not work
 @context('isString')
-//    @stub('underscore','isString').returns(true)
-//    iStringStub: SinonStub
+    @stub('underscore','isString').returns(true)
+    iStringStub: SinonStub
 
     @it('should call _.isString and return true')
     isString(): TR {
-        // expect(isString(4)).to.be.true
-        // expect(this.iStringStub).to.have.been.calledOnce
+        expect(isString(4)).to.be.true
+        expect(this.iStringStub).to.have.been.calledOnce
+    }
+
+@context('isString default')
+    @it('should return false when no stub')
+    isString1(): TR {
+        expect(isString(4)).to.be.false
     }
 }
 
