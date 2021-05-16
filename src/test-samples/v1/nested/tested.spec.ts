90533ee071b45602c63139cd5a5b718882d7a500
Ran Har-Shuv <ran.harshuv@gmail.com>
2021-05-17 00:29:19 +0300

    update to version 1.2.1

--- src/test-samples/v1/nested/tested.spec.ts
+++ src/test-samples/v1/nested/tested.spec.ts
@@ -3,8 +3,9 @@ import { describe, it, TR, pending } from 'soda-test'
 @describe('nested test')
 class NestedTest {
     @it('this is from the nested file')
-    @pending()
+    //@pending()
     dummy(): TR {
         // placeholder
+        console.log('MODE', process.env.MODE)
     }
 }
