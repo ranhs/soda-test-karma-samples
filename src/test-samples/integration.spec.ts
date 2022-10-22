5469ef3fd2e0510bdbe949edf0268cd1582737f5
Ran Har-Shuv <ran.harshuv@gmail.com>
2022-10-22 19:03:25 +0300

    update integration test sample

--- src/test-samples/integration.spec.ts
+++ src/test-samples/integration.spec.ts
@@ -1,4 +1,4 @@
-import { describe, it, expect, TR, TestBed } from 'soda-test'
+import { describe, it, expect, TR, TestBed, ComponentFixture } from 'soda-test'
 import { AppComponent } from 'src/app/app.component';
 
 @describe('integration')
@@ -6,9 +6,17 @@ class IntegrationTest {
 
     @it('got the TestBed')
     addBasic(): TR {
+        expect (TestBed).to.exist
         TestBed.configureTestingModule({
             declarations: [ AppComponent ]
         });
+        let fixture: ComponentFixture<AppComponent>
+        fixture = TestBed.createComponent(AppComponent)
+        expect(fixture).to.exist
+        let component = fixture.componentInstance
+        expect(component).to.exist
+        expect(component.title).to.equal('soda-test-karma-samples')
+
         expect(true).to.be.true
     }
 
