030a9a5b5787dcbde82ea87e180789079581a3a0
Ran Har-Shuv <ran.harshuv@gmail.com>
2022-11-05 00:59:05 +0200

    use the fixture/component decorators

--- src/test-samples/integration.spec.ts
+++ src/test-samples/integration.spec.ts
@@ -1,15 +1,23 @@
-import { describe, it, expect, TR, TestBed, ComponentFixture } from 'soda-test'
+import { describe, it, expect, TR, TestBed, ComponentFixture, fixture, component } from 'soda-test'
 import { AppComponent } from 'src/app/app.component';
 
 @describe('integration')
 class IntegrationTest {
 
+    @fixture(AppComponent)
+    fixture: ComponentFixture<AppComponent>
+    @component(AppComponent)
+    component: AppComponent
+
     @it('got the TestBed')
-    addBasic(): TR {
+    addBasic(
+            @fixture(AppComponent) fixture1: ComponentFixture<AppComponent>,
+            @component(AppComponent) component1: AppComponent
+        ): TR {
         expect (TestBed).to.exist
-        TestBed.configureTestingModule({
-            declarations: [ AppComponent ]
-        });
+        // TestBed.configureTestingModule({
+        //     declarations: [ AppComponent ]
+        // });
         let fixture: ComponentFixture<AppComponent>
         fixture = TestBed.createComponent(AppComponent)
         expect(fixture).to.exist
@@ -17,7 +25,23 @@ class IntegrationTest {
         expect(component).to.exist
         expect(component.title).to.equal('soda-test-karma-samples')
 
-        expect(true).to.be.true
+        expect(this.fixture).to.exist
+        component = this.fixture.componentInstance
+        expect(component).to.exist
+        expect(component.title).to.equal('soda-test-karma-samples')
+        expect(this.component).to.exist
+        expect(this.component.title).to.equal('soda-test-karma-samples')
+        expect(component === this.component).to.be.true
+
+
+        expect(fixture1).to.exist
+        component = fixture1.componentInstance
+        expect(component).to.exist
+        expect(component.title).to.equal('soda-test-karma-samples')
+        expect(component1).to.exist
+        expect(component1.title).to.equal('soda-test-karma-samples')
+        expect(component1 === component).to.be.true
+        expect(component1===this.component).to.be.false
     }
 
 }
